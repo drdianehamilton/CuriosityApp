1b8f3bd4624f66fc01f68c5ce7dbb0f35a6dc666
drdianehamilton <drdianehamilton@gmail.com>
2025-06-06 10:11:01 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
