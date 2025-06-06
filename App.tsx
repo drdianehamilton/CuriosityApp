56d34d2ff3957525640fd4e8f9a866b5d8d9e1b2
drdianehamilton <drdianehamilton@gmail.com>
2025-06-06 11:31:05 -0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
   return (
     <div>
       <h1>Curiosity Code Index Companion</h1>
-      <p>This app measures the impact of FATE (Fear, Assumptions, Technology, Environment) on curiosity.</p>
+      <p>This app measures how Fear, Assumptions, Technology, and Environment impact curiosity.</p>
     </div>
   );
 }
-
 export default App;
