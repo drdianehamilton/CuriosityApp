d4ad8e81c923e109a8f09830236dce29537e2892
drdianehamilton <drdianehamilton@gmail.com>
2025-06-06 12:21:04 -0700

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Curiosity Code Index Companion</h1>
-      <p>This app measures how Fear, Assumptions, Technology, and Environment impact curiosity.</p>
-    </div>
-  );
-}
-export default App;
