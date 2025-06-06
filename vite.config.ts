fccfb43cf839e88db542f960dfb96a26e0db02d0
drdianehamilton <drdianehamilton@gmail.com>
2025-06-06 13:43:15 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true
-  }
-});
+  },
+  root: '.', // 👈 Important: use root as current directory
+  publicDir: 'public', // 👈 Ensures public/index.html is used
+})
