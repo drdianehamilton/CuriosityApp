f597630e1aeff3e03fd3594a67e2183a22cf4669
drdianehamilton <drdianehamilton@gmail.com>
2025-06-06 12:46:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   },
-  plugins: [react()],
-})
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  plugins: [react()]
+});
